ef7a4a03407478dc89b97ceeac2ca866f7cf303c
Gael Pasgrimaud <gael@gawel.org>
2017-10-06 10:29:48 +0200

    Back to development: 0.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='responses_proxy',
-    version='0.1.1',
+    version='0.1.2.dev0',
     description='allow you to easily mock HTTP responses in your tests',
     long_description=long_description,
     url='https://github.com/bearstech/responses_proxy',
